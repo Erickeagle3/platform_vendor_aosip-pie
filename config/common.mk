Lotus-OS/android_vendor_lotus

vendor: Build Recorder app
Change-Id: I5ea986b54139d7666e99020a66a169e5b66b709b

config/common.mk
@@ -100,6 +100,15 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
 
+# Recorder app
+PRODUCT_PACKAGES += \
+    Recorder
+
+# Some more permissions
+PRODUCT_COPY_FILES += \
+    vendor/lotus/prebuilt/common/etc/permissions/privapp-permissions-recorder.xml:system/etc/permissions/privapp-permissions-recorder.xml \
+    vendor/lotus/prebuilt/common/etc/permissions/org.pixelexperience.recorder.xml:system/etc/permissions/org.pixelexperience.recorder.xml
+
 # Power whitelist
 PRODUCT_COPY_FILES += \
     vendor/lotus/config/permissions/custom-power-whitelist.xml:system/etc/sysconfig/custom-power-whitelist.xml

Erickeagle3/platform_vendor_aosip-pie

Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

config/common.mk
@@ -102,6 +102,15 @@ PRODUCT_COPY_FILES += \
     vendor/aosip/prebuilt/common/etc/permissions/privapp-permissions-recorder.xml:system/etc/permissions/privapp-permissions-recorder.xml \
     vendor/aosip/prebuilt/common/etc/permissions/org.pixelexperience.recorder.xml:system/etc/permissions/org.pixelexperience.recorder.xml
 
+# Recorder app
+PRODUCT_PACKAGES += \
+    Recorder
+
+# Some more permissions
+PRODUCT_COPY_FILES += \
+    vendor/aosip/prebuilt/common/etc/permissions/privapp-permissions-recorder.xml:system/etc/permissions/privapp-permissions-recorder.xml \
+    vendor/aosip/prebuilt/common/etc/permissions/org.pixelexperience.recorder.xml:system/etc/permissions/org.pixelexperience.recorder.xml
+
 # Lawnchair
 PRODUCT_PACKAGES += Lawnchair
 PRODUCT_COPY_FILES += \